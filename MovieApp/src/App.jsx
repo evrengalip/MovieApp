dbd83787f1a56a385fc5e40a6b8439c197f358a6
evrengalip <141230714+evrengalip@users.noreply.github.com>
2024-10-23 01:11:51 +0300

    routing

--- MovieApp/src/App.jsx
+++ MovieApp/src/App.jsx
@@ -1,6 +1,13 @@
 
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import Home from './Pages/Home/Home/'
+import MyList from './Pages/MyList/MyList'
+
+import {HOME, MY_LIST} from './constants/path'
+
+
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   
@@ -8,6 +15,12 @@ function App() {
     <div className='app'>
          <Navbar/>
          
+          <Routes>
+              <Route path={HOME} element={<Home/>}/>
+              <Route path={MY_LIST} element={<MyList/>}/>
+
+          </Routes>
+
     </div>
      
 
--- MovieApp/src/components/Navbar/Navbar.jsx
+++ MovieApp/src/components/Navbar/Navbar.jsx
@@ -3,22 +3,34 @@ import { PiFilmReelFill } from "react-icons/pi";
 import { TiHome } from "react-icons/ti";
 import { FaHeart } from "react-icons/fa";
 import './Navbar.css'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className='navbar'>
-        <div className='left'>
-            <h1>MovieApp</h1>
-        </div>
-        <div className='center'>
+      <div className='left'>
+        <Link to="/">
+          <h1>MovieApp</h1>
+        </Link>
+
+      </div>
+      <div className='center'>
         <PiFilmReelFill />
-        </div>
-        <div className='right'>
-            <ul>
-                <li><TiHome/></li>
-                <li><FaHeart/></li>
-            </ul>
-        </div>
+      </div>
+      <div className='right'>
+        <ul>
+          <li>
+            <Link to="/">
+              <TiHome />
+            </Link>
+          </li>
+          <li>
+            <Link to="/my-list">
+              <FaHeart />
+            </Link>
+          </li>
+        </ul>
+      </div>
 
 
     </nav>
